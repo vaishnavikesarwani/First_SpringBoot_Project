2c033fed11b040ea63e28657c51e1ba3da910732
Vaishnavi <kesarwanivaishnavi430@gmail.com>
2023-12-19 22:53:07 +0530

    first commit

---
+++ src/main/java/com/example/firstSpringProject/model/Person.java
@@ -0,0 +1,29 @@
+package com.example.firstSpringProject.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import java.util.UUID;
+
+@EntityScan
+public class Person {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private final UUID id;
+
+    @Column(name = "name")
+    private final String name;
+
+    public Person(@JsonProperty("id") UUID id,
+                  @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
