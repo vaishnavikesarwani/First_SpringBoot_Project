67be1cdd7a62d7a15065148ca7b14f0f4a5863dd
Vaishnavi <kesarwanivaishnavi430@gmail.com>
2024-02-19 14:16:13 +0530

    First Commit

--- src/main/java/com/example/firstSpringProject/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/firstSpringProject/dao/FakePersonDataAccessService.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 
 @Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao{
-    private static List<Person> DB=new ArrayList<>();
+    private static List<Person> DB = new ArrayList<>();
     @Override
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
--- src/main/java/com/example/firstSpringProject/model/Person.java
+++ src/main/java/com/example/firstSpringProject/model/Person.java
@@ -1,13 +1,18 @@
 package com.example.firstSpringProject.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+
 import java.util.UUID;
 
 @EntityScan
 public class Person {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private final UUID id;
 
     @Column(name = "name")
